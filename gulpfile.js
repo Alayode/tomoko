319b41b71d915b8d12945a4cb0902bb0a576b9f9
chris <ksamuel.chris@gmail.com>
2015-02-12 17:09:21 -0500

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 
 gulp.task('sass', function() {
-  gulp.src('public/stylesheets/style.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(gulp.dest('public/stylesheets'));
+    gulp.src('public/stylesheets/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('public/stylesheets'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('public/stylesheets/*.scss', ['sass']);
+    gulp.watch('public/stylesheets/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'watch']);
